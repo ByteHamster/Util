6ed9090dd6109f15d140e4c9d5cff89308b9547f
ByteHamster <info@bytehamster.com>
2022-09-21 16:39:38 +0200

    Add fastrange32

--- include/Function.h
+++ include/Function.h
@@ -40,4 +40,14 @@ static inline uint64_t fastrange64(uint64_t word, uint64_t p) {
 #endif // __SIZEOF_INT128__
 }
 
+/**
+* Given a value "word", produces an integer in [0,p) without division.
+* The function is as fair as possible in the sense that if you iterate
+* through all possible values of "word", then you will generate all
+* possible outputs as uniformly as possible.
+*/
+static inline uint32_t fastrange32(uint32_t word, uint32_t p) {
+    return (uint32_t)(((uint64_t)word * (uint64_t)p) >> 32);
+}
+
 } // Namespace util
