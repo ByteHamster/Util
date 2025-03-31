8672a51ae596911fd0ef3b081bf5d38fa6620182
ByteHamster <info@bytehamster.com>
2025-03-31 11:46:30 +0200

    Add resize support to IntVector

--- include/bytehamster/util/IntVector.h
+++ include/bytehamster/util/IntVector.h
@@ -9,14 +9,21 @@ class IntVector {
     private:
         static constexpr uint64_t MASK = (1ul << elementWidth) - 1;
         static_assert(elementWidth <= 64);
-        const size_t numElements;
+        size_t numElements;
         std::vector<uint64_t> data;
     public:
-        IntVector(size_t size) : numElements(size), data((numElements * elementWidth + 63) / 64) {
-
+        IntVector() : IntVector(0) {
         }
 
-        [[nodiscard]] uint64_t at(size_t index) const {
+        explicit IntVector(const size_t size) : numElements(size), data((numElements * elementWidth + 63) / 64) {
+        }
+
+        void resize(const size_t size) {
+            numElements = size;
+            data.resize((numElements * elementWidth + 63) / 64);
+        }
+
+        [[nodiscard]] uint64_t at(const size_t index) const {
             const size_t bitIdx = index * elementWidth;
             const uint8_t offset = bitIdx % 64;
             const uint64_t* word = &data[bitIdx / 64];
@@ -28,7 +35,7 @@ class IntVector {
             }
         }
 
-        void set(size_t index, uint64_t value) {
+        void set(const size_t index, uint64_t value) {
             const size_t bitIdx = index * elementWidth;
             uint64_t* word = &data[bitIdx / 64];
             const uint8_t offset = bitIdx % 64;
