57b32e868bf54e4c31fd8498dbe4f0454e83f008
ByteHamster <info@bytehamster.com>
2025-04-07 15:44:29 +0200

    Fix support for elementWidth=0

--- include/bytehamster/util/IntVector.h
+++ include/bytehamster/util/IntVector.h
@@ -7,8 +7,8 @@ namespace bytehamster::util {
 template <uint8_t elementWidth>
 class IntVector {
     private:
-        static constexpr uint64_t MASK = (1ul << elementWidth) - 1;
         static_assert(elementWidth <= 64);
+        static constexpr uint64_t MASK = elementWidth == 64 ? ~0ul : ((1ul << elementWidth) - 1);
         size_t numElements;
         std::vector<uint64_t> data;
     public:
@@ -24,27 +24,37 @@ class IntVector {
         }
 
         [[nodiscard]] uint64_t at(const size_t index) const {
-            size_t bitIdx = index * elementWidth;
-            uint8_t offset = bitIdx % 64;
-            const uint64_t* word = &data[bitIdx / 64];
-            uint64_t w1 = (*word) >> offset;
-            if (offset + elementWidth > 64) {
-                return (w1 | (*(word+1) << (64 - offset))) & MASK;
+            if constexpr (elementWidth == 0) {
+                return 0;
+            } else if constexpr (elementWidth == 64) {
+                return data[index];
             } else {
-                return w1 & MASK;
+                const size_t bitIdx = index * elementWidth;
+                const size_t offset = bitIdx % 64;
+                const uint64_t* word = &data[bitIdx / 64];
+                uint64_t w1 = (*word) >> offset;
+                if (offset + elementWidth > 64) {
+                    return (w1 | (*(word+1) << (64 - offset))) & MASK;
+                } else {
+                    return w1 & MASK;
+                }
             }
         }
 
         void set(const size_t index, uint64_t value) {
-            const size_t bitIdx = index * elementWidth;
-            uint64_t* word = &data[bitIdx / 64];
-            const uint8_t offset = bitIdx % 64;
-            value &= MASK;
-            *word &= ~(MASK << offset);
-            *word |= (value << offset);
-            if (offset + elementWidth >= 64) {
-                *(word+1) &= ~(MASK >> (64 - offset));
-                *(word+1) |= (value >> (64 - offset));
+            if constexpr (elementWidth == 64) {
+                data[index] = value;
+            } else if constexpr (elementWidth > 0) {
+                const size_t bitIdx = index * elementWidth;
+                uint64_t* word = &data[bitIdx / 64];
+                const size_t offset = bitIdx % 64;
+                value &= MASK;
+                *word &= ~(MASK << offset);
+                *word |= (value << offset);
+                if (offset + elementWidth >= 64) {
+                    *(word+1) &= ~(MASK >> (64 - offset));
+                    *(word+1) |= (value >> (64 - offset));
+                }
             }
         }
 
